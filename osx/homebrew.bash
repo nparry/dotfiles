96c34d48f33425c5c4cd5fb06a80dba4a1943e44
Nathan Parry <nparry@gmail.com>
2013-03-23 22:43:40 -0400

    Other homebrew path stuff

--- osx/homebrew.bash
+++ osx/homebrew.bash
@@ -3,7 +3,7 @@ if which brew >/dev/null; then
     . $(brew --prefix)/etc/bash_completion
   fi
 
-  export PATH="$(brew --prefix)/bin:$PATH"
+  export PATH="$(brew --prefix)/bin:$(brew --prefix)/sbin:$PATH"
   export MANPATH="$(brew --prefix)/share/man:$MANPATH"
 fi
 
