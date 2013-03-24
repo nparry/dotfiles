101df1e99ae66f1af99b70241300f0e3451e7b02
Nathan Parry <nparry@gmail.com>
2013-03-23 18:01:57 -0700

    Work on Linux too

--- ruby/rbenv.bash
+++ ruby/rbenv.bash
@@ -1,3 +1,8 @@
-export RBENV_ROOT=$(brew --prefix)/opt/rbenv
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+if which brew >/dev/null; then
+  export RBENV_ROOT=$(brew --prefix)/opt/rbenv
+elif [ -d $HOME/.rbenv ]; then
+  export PATH="$HOME/.rbenv/bin:$PATH"
+fi
+
+if which rbenv >/dev/null; then eval "$(rbenv init -)"; fi
 
