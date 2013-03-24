a6dad4cfd3280c467ab339bd8efdb22cf63646ff
Nathan Parry <nparry@gmail.com>
2013-03-23 20:39:05 -0700

    Fix rbenv on Dreamhost

--- ruby/rbenv.bash
+++ ruby/rbenv.bash
@@ -4,5 +4,22 @@ elif [ -d $HOME/.rbenv ]; then
   export PATH="$HOME/.rbenv/bin:$PATH"
 fi
 
-if which rbenv >/dev/null; then eval "$(rbenv init -)"; fi
+if which rbenv >/dev/null; then
+  # Fix Dreamhost strangeness
+  unset GEM_HOME
+  unset GEM_PATH
+
+  # Strip system gem stuff from the path
+  # http://stackoverflow.com/questions/370047/what-is-the-most-elegant-way-to-remove-a-path-from-the-path-variable-in-bash
+  export PATH=$(
+    IFS=:
+    t=($PATH)
+    unset IFS
+    t=(${t[@]%%*gems*})
+    IFS=:
+    echo "${t[*]}"
+  )
+
+  eval "$(rbenv init -)"
+fi
 
