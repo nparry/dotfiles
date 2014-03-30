860e949253bbd5190c68fe609bfe8c7b2a7b1659
Nathan Parry <nparry@gmail.com>
2014-03-30 11:53:45 -0400

    Powerline all the things

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -1,2 +1,7 @@
-export PS1='\[\033[0;33m\]\u@\h\[\033[0m\]:\W$(__git_ps1 "(\[\033[0;37m\]%s\[\033[0m\])") ➡ \[\033[0m\]'
+POWERLINE_CONFIG="/usr/local/lib/python2.7/site-packages/powerline/bindings/bash/powerline.sh"
+if [ -f "$POWERLINE_CONFIG" ]; then
+  source "$POWERLINE_CONFIG"
+else
+  export PS1='\[\033[0;33m\]\u@\h\[\033[0m\]:\W$(__git_ps1 "(\[\033[0;37m\]%s\[\033[0m\])") ➡ \[\033[0m\]'
+fi
 
